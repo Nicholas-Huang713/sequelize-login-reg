8a132caeb23b1253211098670d0397020d014295
Nicholas_Huang713 <nhuang713@gmail.com>
2020-04-13 04:34:50 -0700

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPost = {text}
+    axios.post('/api/new', newPost)
+      .then(() => {
+        console.log("success")
+        axios.get('/api/all')
+          .then((res) => {
+            setTodos(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    
+  }
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setText(value);
+  }
+
+  // useEffect(() => {
+  //   function handleRetrieveTodos(){
+
+  //   }
+  //   axios.get('/api/all')
+  //     .then((res) => {
+  //       setTodos(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     })
+  // });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleChange}/>
+        <button>Submit</button>
+      </form>
+      <ul>
+        {
+          todos.map((todo) => {
+            return (
+              <li>{todo.text}</li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
